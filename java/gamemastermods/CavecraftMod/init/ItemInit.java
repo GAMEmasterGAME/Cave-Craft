470be336dfb566ea84e6774b8acaa91e62b392c0
GAMEmasterGAME <46026687+GAMEmasterGAME@users.noreply.github.com>
2018-12-28 11:04:41 +0100

    Add files via upload

---
+++ java/gamemastermods/CavecraftMod/init/ItemInit.java
@@ -0,0 +1,14 @@
+package gamemastermods.CavecraftMod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gamemastermods.CavecraftMod.objects.items.ItemBase;
+import net.minecraft.item.Item;
+
+public class ItemInit {
+
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+ 
+	public static final Item COPPER_INGOT = new ItemBase("ingot_copper");
+}
